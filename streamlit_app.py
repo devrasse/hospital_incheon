58854b618710fcc2df36bc69248156e6a5b6cab5
jongho Baek <decrasse9001@gmail.com>
2024-02-03 15:42:41 +0900

    '1'

---
+++ streamlit_app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(layout='wide')
+# HTML 파일을 읽어오기
+with open("hospital_incheon.html", "r",  encoding="utf-8") as f:
+    html_code = f.read()
+
+st.markdown('<div class="centered"><h1 style="text-align:center;">🏥 인천시 설 명절 비상진료 🏥</h1></div>', unsafe_allow_html=True)
+
+with st.container(border=True,height=740):
+    # Stremlit 앱에 HTML 표시
+    st.components.v1.html(html_code,  height=700, scrolling=False)
+
+#st.write("참고: 정확한 진료 확인은 방문전 연락하여 확인하여 주시기 바랍니다.")
+styled_text = '<p style="font-size: 20px; color: white;">참고: 정확한 진료 확인은 방문전 연락하여 확인하여 주시기 바랍니다.</p>'
+st.write(styled_text, unsafe_allow_html=True)
