23c4512ead866b2f730a9e72b982a7c6e78a52d3
devrasse <129311690+devrasse@users.noreply.github.com>
2024-02-05 14:02:30 +0900

    Update streamlit_app.py
    3

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,8 @@ st.markdown('<div class="centered"><h1 style="text-align:center;">🏥 인천시
 
 with st.container(border=True,height=740):
     # Stremlit 앱에 HTML 표시
-    st.components.v1.html(html_code,  height=700, scrolling=False)
+    with st.spinner("로딩 중..."):
+        st.components.v1.html(html_code,  height=700, scrolling=False)
 
 #st.write("참고: 정확한 진료 확인은 방문전 연락하여 확인하여 주시기 바랍니다.")
 styled_text = '<p style="font-size: 20px; color: white;">참고: 정확한 진료 확인은 해당 병원에 문의하여 주시기 바랍니다.</p>'
